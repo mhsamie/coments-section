2c865b87d3f0c2e8aaf338df11d97b90c5849eb7
mhsamie <sfatima7720@gmail.com>
2022-07-22 23:41:10 +0430

    style added, rendering bug fixed, full comments added

--- src/components/comment/FullComment.js
+++ src/components/comment/FullComment.js
@@ -1,28 +1,33 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-
+import "./fullcomment.css";
 const FullComment = ({ commentId }) => {
   const [comment, setComment] = useState(null);
   useEffect(() => {
-    axios
-      .get(`https://jsonplaceholder.typicode.com/comments/${commentId}`)
-      .then((response) => setComment(response.data));
+    if (commentId) {
+      axios
+        .get(`https://jsonplaceholder.typicode.com/comments/${commentId}`)
+        .then((response) => setComment(response.data));
+    }
   }, [commentId]);
-  if (!commentId) return <p>You didn't choose any comment to see</p>;
-  return (
-    <div>
-      {" "}
-      {comment ? (
-        <>
-          <p>name: {comment.name}</p>
-          <p>email: {comment.email}</p>
-          <p>body:{comment.body}</p>
-        </>
-      ) : (
-        <p>no cm</p>
-      )}
-    </div>
-  );
+  let commentDetail = <p> You didn't choose a comment to see!</p>;
+  if (commentId) commentDetail = <p>loading...</p>;
+  if (comment) {
+    commentDetail = (
+      <div className="full-comment">
+        <p>
+          name: <span>{comment.name}</span>
+        </p>
+        <p>
+          email:<span> {comment.email}</span>
+        </p>
+        <p>
+          body:<span>{comment.body}</span>
+        </p>
+      </div>
+    );
+  }
+  return commentDetail;
 };
 
 export default FullComment;
